0b5ab1f9f3c49f529d4cc2fc58c649315c822911
cympfh <cympfh@gmail.com>
2018-03-10 21:03:09 +0900

    cron.sh every 1hr

--- cron.sh
+++ cron.sh
@@ -44,13 +44,26 @@ work() {
 }
 
 while :; do
+    atq 2:00 echo && work
+    atq 4:00 echo && work
+    atq 6:00 echo && work
+    atq 7:00 echo && work
     atq 8:00 echo && work
+    atq 9:00 echo && work
     atq 10:00 echo && work
+    atq 11:00 echo && work
     atq 12:00 echo && work
+    atq 13:00 echo && work
     atq 14:00 echo && work
+    atq 15:00 echo && work
     atq 16:00 echo && work
+    atq 17:00 echo && work
     atq 18:00 echo && work
+    atq 19:00 echo && work
     atq 20:00 echo && work
+    atq 21:00 echo && work
     atq 22:00 echo && work
+    atq 23:00 echo && work
+    atq 24:00 echo && work
     sleep 3h
 done
