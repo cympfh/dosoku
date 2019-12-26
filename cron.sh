87a7de3bd10a70de987c63d7d696d52a90cbd709
cympfh <cympfh@gmail.com>
2019-12-26 23:50:02 +0900

    :art:

--- cron.sh
+++ cron.sh
@@ -25,6 +25,7 @@ post() {
     ]
 }
 EOM
+cat $PAYLOAD
     curl "$URL" --data @$PAYLOAD
     rm $PAYLOAD
 }
@@ -38,17 +39,29 @@ post-anime() {
     post animetick tv "#44aa88" "$(animetick -D)"
 }
 
+post-kakaku() {
+    kakaku=$(
+        curl -s http://cameranonaniwa.jp/shop/g/g2111012202684/ | nkf |
+            grep -A 1 price_sale_ | tail -1 |
+            sed 's/^[ \t\n\r]*//g; s/[ \t\r\n]*$//g'
+    )
+    echo $kakaku
+    post kakaku moneybag "#555533" "$kakaku"
+}
+
 work() {
     post-tenki
     post-anime
+    post-kakaku
 }
 
+work
 while :; do
     atq 2:00 echo && work
     atq 4:00 echo && work
     atq 6:00 echo && work
-    atq 7:00 echo && work
     atq 8:00 echo && work
+    atq 8:30 echo && work
     atq 9:00 echo && work
     atq 10:00 echo && work
     atq 11:00 echo && work
@@ -60,10 +73,15 @@ while :; do
     atq 17:00 echo && work
     atq 18:00 echo && work
     atq 19:00 echo && work
+    atq 19:30 echo && work
     atq 20:00 echo && work
+    atq 20:30 echo && work
     atq 21:00 echo && work
+    atq 21:30 echo && work
     atq 22:00 echo && work
+    atq 22:30 echo && work
     atq 23:00 echo && work
+    atq 23:30 echo && work
     atq 24:00 echo && work
     sleep 3h
 done
